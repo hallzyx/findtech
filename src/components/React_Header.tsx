721edd7966ef5e994e96aead9f3947044eea1aeb
hallzyx <hallinxsl7@gmail.com>
2024-09-21 21:24:19 -0500

    feat: header, hero and benefits component - index.astro

---
+++ src/components/React_Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const elementos=[
+    {
+        name:"Afiliados",
+        path:"#"
+    },
+    {
+        name:"Sobre nosotros",
+        path:"#"
+    },
+    {
+        name:"Soporte tecnico",
+        path:"#"
+    }    
+]
+
+
+export const React_Header = () => {
+  return (
+    <header className='py-7 shadow-md'>
+        <div className='container mx-auto flex justify-between items-center'>
+            <img src="profile.png" className='h-8' alt="logo"/>
+            <nav>
+                <ul className='flex gap-12'>
+                    {elementos.map((item)=>{
+                        return(
+                            <li className='font-link text-xl'><a className='text-[#a97f97] hover:text-[#d4976d] transition-all' href={item.path}>{item.name}</a></li>         
+                        )
+                        
+                    })}
+                    {/* <li>
+                        <button>
+                            Mas Opciones
+                        </button>
+                    </li> */}
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
